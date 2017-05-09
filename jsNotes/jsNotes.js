241f3bfe8ed3b57a0e6bc8e012cca32b3e51f369
Philippe <philippesdixon@gmail.com>
2017-05-09 13:11:11 -0400

    Added JavaScript code

---
+++ jsNotes/jsNotes.js
@@ -0,0 +1,13 @@
+//Need to go back and do prior notes
+
+//Conditional Statements
+//to declare:
+    //if () {
+    //    code
+    //}     <-- No ';' after closing brace
+//example:
+var answer = prompt('What programming language is the name of a gem?');
+if (answer === 'Ruby') {
+    document.write("<p>That's right!</p>");
+}
+
