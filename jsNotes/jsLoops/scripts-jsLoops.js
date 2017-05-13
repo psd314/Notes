fde9cc40ea497c53f8922ee09230a8f2d641cb80
Philippe <philippesdixon@gmail.com>
2017-05-13 18:08:26 -0400

    finished jsLoops

--- jsNotes/jsLoops/scripts-jsLoops.js
+++ jsNotes/jsLoops/scripts-jsLoops.js
@@ -65,6 +65,69 @@ function getRandomNumber(upper) {
 //    document.write('<h1>Sorry. You did not guess the number.');
 //}
 
+var person = {
+    name : 'Sarah',
+    country : 'US',
+    age : 35,
+    treehouseStudent : true,
+    skills : ['JavaScript', 'HTML', 'CSS']
+    };
+    
+function print(message) {
+    var div = document.getElementById('output');
+    div.innerHTML = message;
+}
+
+//var message = '<p>Hello. My name is ' + person.name + '</p>';
+//message += '<p>I live in the ' + person.country + '</p>';
+//person.name = 'Rainbow Dash';
+//message += '<p>But, I wish my name was ' + person.name + '</p>';
+//person.age += 1;
+//message += '<p>My age is now ' + person.age + '</p>';
+//message += '<p>I have ' + person.skills.length + ' skills: ';
+//message += person.skills.join(', ') + '</p>';
+//
+//
+//print(message);
+
+//for ( key in person) {
+//    console.log(key, ': ', person[key]);
+//}
+
+var questions = [
+    {
+        question: 'How many states are in the US?',
+        answer: 50
+    },
+    {
+        question: 'How many continents are there?',
+        answer: 7
+    },
+    {
+        question: 'How many legs does an insect have?',
+        answer: 6
+    }
+];
+
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+
+for (var i = 0; i < questions.length; i += 1) {
+    question = questions[i].question;
+    answer = questions[i].answer;
+    response = prompt(question);
+    response = parseInt(response);
+    if (response === answer) {
+        correctAnswers += 1;
+    }
+}
+
+html = "You got " + correctAnswers + " questions(s) right.";
+print(html);
+
+
 
 
 
